bbe2225abd45cf84aec35369ca657ed0fdef651f
ho <ws000617@gmail.com>
2023-08-12 10:24:55 +0200

    Add Comment

--- module04/RacingGame/racinggame.cpp
+++ module04/RacingGame/racinggame.cpp
@@ -2,9 +2,13 @@
 #include <QPainter>
 #include <QApplication>
 
+// PlayerInputThread Constructor
+// playerNum: The player number (1 or 2)
+// keyState: A reference to a map containing the current state of key presses
 PlayerInputThread::PlayerInputThread(int playerNum, QMap<int, bool> &keyState)
     : playerNum(playerNum), keyState(keyState) {}
 
+// Thread run function, checks for key presses and emits a signal to move the player
 void PlayerInputThread::run() {
     while(true) {
         int direction = -1;
@@ -14,28 +18,30 @@ void PlayerInputThread::run() {
             else if(keyState[Qt::Key_A]) direction = 2;  // Left
             else if(keyState[Qt::Key_D]) direction = 3;  // Right
         } else {
-            if(keyState[Qt::Key_Up]) direction = 0;
-            else if(keyState[Qt::Key_Down]) direction = 1;
-            else if(keyState[Qt::Key_Left]) direction = 2;
-            else if(keyState[Qt::Key_Right]) direction = 3;
+            if(keyState[Qt::Key_Up]) direction = 0;  // Up
+            else if(keyState[Qt::Key_Down]) direction = 1;  // Down
+            else if(keyState[Qt::Key_Left]) direction = 2;  // Left
+            else if(keyState[Qt::Key_Right]) direction = 3;  // Right
         }
 
-        if(direction != -1) emit movePlayer(playerNum, direction);
+        if(direction != -1)
+            emit movePlayer(playerNum, direction);
 
-        QThread::msleep(50);
+        QThread::msleep(50); // Sleep to prevent excessive CPU usage
     }
 }
 
+// RacingGame Constructor
 RacingGame::RacingGame(QWidget *parent) : QWidget(parent) {
-    this->setFixedSize(400, 400);
+    this->setFixedSize(400, 400); // Set window size
 
     player1 = new QLabel(this);
     player2 = new QLabel(this);
     endLabel = new QLabel(this);
 
+    // Configure players and label
     player1->setGeometry(10, 190, 20, 20);
     player1->setStyleSheet("background-color: red;");
-
     player2->setGeometry(10, 190, 20, 20);
     player2->setStyleSheet("background-color: blue;");
 
@@ -45,16 +51,19 @@ RacingGame::RacingGame(QWidget *parent) : QWidget(parent) {
     endLabel->setFont(font);
     endLabel->hide();
 
+    // Create threads and connect signals to slots
     player1Thread = new PlayerInputThread(1, keyState);
     player2Thread = new PlayerInputThread(2, keyState);
 
     connect(player1Thread, &PlayerInputThread::movePlayer, this, &RacingGame::onMovePlayer);
     connect(player2Thread, &PlayerInputThread::movePlayer, this, &RacingGame::onMovePlayer);
 
+    // Start player threads
     player1Thread->start();
     player2Thread->start();
 }
 
+// Destructor, terminates threads
 RacingGame::~RacingGame() {
     player1Thread->terminate();
     player2Thread->terminate();
@@ -62,73 +71,71 @@ RacingGame::~RacingGame() {
     delete player2Thread;
 }
 
+// Key press event handler, sets the state of the pressed key to true
 void RacingGame::keyPressEvent(QKeyEvent *event) {
     keyState[event->key()] = true;
 }
 
+// Key release event handler, sets the state of the released key to false
 void RacingGame::keyReleaseEvent(QKeyEvent *event) {
     keyState[event->key()] = false;
 }
 
+// Slot to move a player, called when a direction key is pressed
 void RacingGame::onMovePlayer(int playerNum, int direction) {
     QLabel *player = (playerNum == 1) ? player1 : player2;
     int x = player->x();
     int y = player->y();
 
-    switch(direction) {
-        case 0:  // Up
-            y -= 5;
-            break;
-        case 1:  // Down
-            y += 5;
-            break;
-        case 2:  // Left
-            x -= 5;
-            break;
-        case 3:  // Right
-            x += 5;
-            break;
+    switch(direction) { // Adjust coordinates based on direction
+        case 0: y -= 5; break; // Up
+        case 1: y += 5; break; // Down
+        case 2: x -= 5; break; // Left
+        case 3: x += 5; break; // Right
     }
 
     player->move(x, y);
-    checkBoundary();
-    checkWinCondition();
+    checkBoundary(); // Check if the player is out of bounds
+    checkWinCondition(); // Check if a player has won
 }
 
+// Paint event handler, draws the boundaries and starting/finishing lines
 void RacingGame::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
     QPen pen(Qt::black, 2);
 
     painter.setPen(pen);
 
-    // 경계를 그립니다.
+    // Draw boundaries
     painter.drawRect(0, 0, 400, 400);
 
-    // 출발선과 도착선을 그립니다.
-    painter.drawLine(10, 0, 10, 400);  // 출발선
-    painter.drawLine(390, 0, 390, 400);  // 도착선
+    // Draw starting and finishing lines
+    painter.drawLine(10, 0, 10, 400); // Starting line
+    painter.drawLine(390, 0, 390, 400); // Finishing line
 
     QWidget::paintEvent(event);
 }
 
+// Check if a player is out of bounds, end the game if so
 void RacingGame::checkBoundary() {
     if (player1->x() < 0 || player1->x() > this->width() ||
         player1->y() < 0 || player1->y() > this->height()) {
-        endLabel->setText("Player 2 Wins!");
+        endLabel->setText("Blue Wins!");
         endLabel->show();
     } else if (player2->x() < 0 || player2->x() > this->width() ||
                player2->y() < 0 || player2->y() > this->height()) {
-        endLabel->setText("Player 1 Wins!");
+        endLabel->setText("Red Wins!");
         endLabel->show();
     }
 }
 
+// Check if a player has reached the finish line, end the game if so
 void RacingGame::checkWinCondition() {
     if (player1->x() > 380 && player1->y() > 180 && player1->y() < 220) {
-        endLabel->setText("Player 1 Wins!");
+        endLabel->setText("Red Wins!");
         endLabel->show();
     } else if (player2->x() > 380 && player2->y() > 180 && player2->y() < 220) {
-        endLabel->setText("Player 2 Wins!");
+        endLabel->setText("Blue Wins!");
         endLabel->show();
     }
 }
--- module04/RacingGame/racinggame.h
+++ module04/RacingGame/racinggame.h
@@ -7,46 +7,62 @@
 #include <QThread>
 #include <QKeyEvent>
 
+// PlayerInputThread class handles player input in a separate thread
 class PlayerInputThread : public QThread {
     Q_OBJECT
 
 public:
+    // Constructor with player number and a reference to the key state map
     PlayerInputThread(int playerNum, QMap<int, bool> &keyState);
 
 signals:
+    // Signal to move the player
     void movePlayer(int playerNum, int direction);
 
 protected:
+    // Override QThread's run function
     void run() override;
 
 private:
-    int playerNum;
-    QMap<int, bool> &keyState;
+    int playerNum;                // Player number (1 or 2)
+    QMap<int, bool> &keyState;   // Reference to the key state map
 };
 
+// RacingGame class represents the main game
 class RacingGame : public QWidget {
     Q_OBJECT
 
 public:
+    // Constructor and Destructor
     RacingGame(QWidget *parent = nullptr);
     ~RacingGame();
+
+    // Overridden from QWidget to handle custom drawing
     void paintEvent(QPaintEvent *event) override;
+
+    // Overridden from QWidget to handle key press events
     void keyPressEvent(QKeyEvent *event) override;
+
+    // Overridden from QWidget to handle key release events
     void keyReleaseEvent(QKeyEvent *event) override;
 
 public slots:
+    // Slot to handle player movement
     void onMovePlayer(int playerNum, int direction);
 
 private:
-    QLabel *player1;
-    QLabel *player2;
-    QLabel *endLabel;
-    PlayerInputThread *player1Thread;
-    PlayerInputThread *player2Thread;
+    QLabel *player1;               // Player 1 label
+    QLabel *player2;               // Player 2 label
+    QLabel *endLabel;              // Label to display the winner
+    PlayerInputThread *player1Thread; // Thread for player 1 input
+    PlayerInputThread *player2Thread; // Thread for player 2 input
 
-    QMap<int, bool> keyState;
+    QMap<int, bool> keyState;     // Map to store the state of pressed keys
 
+    // Function to check if a player is out of bounds
     void checkBoundary();
+
+    // Function to check if a player has won
     void checkWinCondition();
 };
 
